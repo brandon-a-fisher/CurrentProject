3c474a2f7bdf213dc8f6df6f86ccc9a108f86de7
brandon-a-fisher <35775975+brandon-a-fisher@users.noreply.github.com>
2018-02-05 16:47:13 -0700

    Update Player.java

--- Main Project/Player/Player.java
+++ Main Project/Player/Player.java
@@ -1,19 +1,27 @@
 public class Player
 {
 	private int playerHealth = 5;
-	private int xPos = 0;
-	private int yPos = 0;
+	private int xValue;
+	private int yValue;
+	private boolean isAlive;
 
-	public boolean IsAlive()
+	public Player()
 	{
-		if(playerHealth<=0)
-		{
-			return false;
-		}
-		else
-		{
-			return true;
-		}
+		xValue = 0;
+		yValue = 10;
+		isAlive = true;
+	}
+	
+	public Player(int setX, int setY)
+	{
+		xValue = setX;
+		yValue = setY;
+		isAlive = true;
+	}
+	
+	public boolean getIsAlive()
+	{
+		return isAlive;
 	}
 
 	public void TakeDamage()
@@ -27,5 +35,20 @@ public class Player
 		{
 			playerHealth += 1;
 		}
+	}
+	
+	public void Jump()
+	{
+		pass;
+	}
+	
+	public getXPos()
+	{
+		return xValue;
+	}
+	
+	public getYPos()
+	{
+		return yValue;
 	}
 }
