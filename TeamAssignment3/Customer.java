6efa7034740378e99a451d509af622cb2fa42ae0
Mark <35700344+markjt17@users.noreply.github.com>
2018-02-05 15:59:36 -0700

    fix

--- TeamAssignment3/Customer.java
+++ TeamAssignment3/Customer.java
@@ -3,34 +3,29 @@ public class Customer
 	private String name;
 	private int customerID ;
 	
-  	/**
-	  * The following constructor acts as the default constructor that does not take in any arguments.
-	  */
+  /**
+	* The following are the constructors for the Customer
+	*/
 	public Customer()
 	{
 		name = "No Name";
 		customerID  = 0;
 	}
 	
-	/**
-	  * Here, the constructor takes in an initial name and an initial ID.
-	  */
 	public Customer(String initialName, int initialID)
 	{
 		name = initialName;
 		customerID = initialID;
 	}
 	
-	/**
-	  * This constructor will take in the information of a previous customer, acting as a copy.
-	  */
 	public Customer(Customer oldCustomer)
 	{
 		name = oldCustomer.getName();
 		customerID = oldCustomer.getID();
 	}
 	/** 
- 	  * The following four methods are the getter and setter methods for the instance variables name and customerID.
+ 	  * The following four methods are the getter and setter methods for
+	  * the instance variables name and customerID.
 	  */
 	public String getName()
 	{
@@ -53,11 +48,17 @@ public class Customer
 	}
 	
 	/**
-	  * Returns a string containing a formatted output of the Customer name and Customer ID.
+	  *Returns a string containing a formatted output of the Customer
+	  *name and Customer ID.
 	  */
 	public String toString()
 	{
 		String customerString = "CustomerName: " + name + " , CustomerID: " + customerID;
 		return customerString;
+	}
+	
+	public static void main(String[]args)
+	{
+
 	}
 }
