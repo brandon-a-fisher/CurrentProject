a7bade63f7552445f3f170e0b92cc65cfc2abb53
brandon-a-fisher <35775975+brandon-a-fisher@users.noreply.github.com>
2018-02-07 16:54:42 -0700

    Add files via upload

---
+++ Main Project/AnimationApplicationClass/TextOutput.java
@@ -0,0 +1,38 @@
+
+public class TextOutput
+{
+	public int[] activePlayers = new int[0];
+	public int[] activeObstacles = new int[0];
+	public int[] activeCollectibles = new int[0];
+
+	public void PrintActivePositions(AnimationApplication gameEngine)
+	{
+
+
+		activePlayers = gameEngine.stepThroughActive("Player");
+		activeObstacles = gameEngine.stepThroughActive("Obstacle");
+		activeCollectibles = gameEngine.stepThroughActive("Collectible");
+
+		for(int index = 0; index < activePlayers.length; index++)
+		{
+			System.out.print("Player" + index + "(x, y): " + gameEngine.getPlayer(activePlayers[index]).getXPosition() +
+			" " + gameEngine.getPlayer(activePlayers[index]).getYPosition());
+		}
+
+		System.out.println();
+		
+		for(int index = 0; index < activeObstacles.length; index++)
+		{
+			System.out.print("Obstacle" + index + "(x, y): " + gameEngine.getObstacle(activeObstacles[index]).getXPosition() +
+			" " + gameEngine.getObstacle(activeObstacles[index]).getYPosition());
+		}
+
+		System.out.println();
+
+		for(int index = 0; index < activeCollectibles.length; index++)
+		{
+			System.out.print("Collectible" + index + "(x, y): " + gameEngine.getCollectible(activeCollectibles[index]).getXPosition() +
+			 " " + gameEngine.getCollectible(activeCollectibles[index]).getYPosition());
+		}
+	}
+}
