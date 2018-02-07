dc69d309b4b6dd1c9261830c19ad8f0fb7e1aaa4
BluSnowY <35359631+BluSnowY@users.noreply.github.com>
2018-02-07 16:43:10 -0700

    Update Obstacle.java

--- Main Project/AnimationApplicationClass/Obstacle.java
+++ Main Project/AnimationApplicationClass/Obstacle.java
@@ -1,107 +1,72 @@
-public class Obstacle
-{
-	
-	/*
-	 * Here, the variables of the obstacle are initiated.
-	 */
-	private boolean isAlive = true;
-	
-	private int xValue = 30;
-	private int yValue;
+/**
+Creates an object with its length and height an x and y position aIt moves to
+the left of the screen. It checks if it overlaps with the player in the game or
+if the object if off-screen.
+*/
+public class Collectible {
 
+	private int xValue;
+	private int yValue;
 	private int length;
 	private int height;
-	
-	/*
-	 * This is the default constructor. It creates an obstacle of size one for length
-	 *   and height.
-	 */
-	public Obstacle() 
-	{
-		yValue = 1;
-				
-		length = 1;
-		height = 1;
-	}
-	
-	/*
-	 * The following constructor allows for the maximum amount of customization. It allows
-	 *   the creation of an obstacle with a new yValue, height, and length.
-	 */
-	public Obstacle( int newYValue, int newLengthValue, int newHeightValue )
-	{
+
+/**
+Constuctor requiring a y value to spawn.
+*/
+	public Collectible(int newYValue) {
 		yValue = newYValue;
-		
-		length = newLengthValue;
-		height = newHeightValue;
-	}
-	
-	/*
-	 * The final constructor only allows for the change of the yValue, while the height and
-	 *   length stay at one.
-	 */
-	public Obstacle( int newYValue )
-	{
-		yValue = newYValue;
-		
-		length = 1;
+		xValue = 30;
 		height = 1;
+		length = 1;
 	}
-	
-	/*
-	 * moveObstaclePosition moves the obstacle to the left by one unit each time it
-	 *   is called.
-	 */
-	public void moveObstacle()
-	{
-		xValue -= 1;
-	}
-	
-	/*
-	 * The following three getter methods return the xValue, yValue, and isAlive of the obstacle.
-	 */
-	public int getXPosition()
-	{
-		return xValue;
-	}
-	
-	public int getYPosition()
-	{
+
+/**
+Returns the y position.
+*/
+	public int getYPosition() {
 		return yValue;
 	}
 
-	public boolean getIsAlive()
-	{
+/**
+Returns the y position.
+*/
+	public int getXPosition() {
+		return xValue;
+	}
+
+/**
+Returns a boolean of the isAlive.
+*/
+	public int getIsAlive() {
 		return isAlive;
 	}
-	
-	/*
-	 * hitObstacle checks whether or not the player has hit the obstacle and returns a
-	 *   value of true or false accordingly. It also changes the value of isAlive.
-	 */
-	public boolean overlapsWith( int xPos, int yPos )
-	{
-		if ( (xValue == xPos) && (yValue == yPos ) )
-		{
+
+/**
+Returns true if the objects overlaps the given x and y positions and false if
+not.
+*/
+	public boolean overlapsWith(int yPos, int xPos) {
+		if (xPos == this.xValue && yPos == this.yValue)
 			isAlive = false;
 			return true;
-		}
-		
 		else
-		{
 			return false;
-		}
 	}
-	
-	/*
-	 * obstacleIsGone checks whether or not the obstacle has passed the screen and changes
-	 *  the value of isAlive accordingly.
-	 */
-	public void obstacleGone()
-	{
-		if ( xValue < 0 )
-		{
-			isAlive = false;
-		}
+
+/**
+Returns true if the collectible goes off-screen based on the x position.
+*/
+	public boolean collectibleGone() {
+		if (xValue < 0 )
+			return true;
+		else
+			return false;
+	}
+
+/**
+Moves the collectible towards the left.
+*/
+	public void moveCollectible(){
+		xValue --;
 	}
 }
