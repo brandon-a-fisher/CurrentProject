d23bc108c513891a5482474425742a43fcbf71f9
BluSnowY <35359631+BluSnowY@users.noreply.github.com>
2018-02-12 13:26:55 -0700

    Update SavingsAccount.java

--- Team Assignments/Team Assignment 4/SavingsAccount.java
+++ Team Assignments/Team Assignment 4/SavingsAccount.java
@@ -12,29 +12,51 @@ public class SavingsAccount extends BankAccount{
 	private double annualInterestRate; // Assumes a Positive %
 	private BankAccount bankAccount;
 
+/**
+Constructor using only the base class
+*/
 	SavingsAccount () {
 		super ();
 	}
-	
+
+/**
+Constructor using the base class with a given rate
+*/
 	SavingsAccount (double rate) {
-		super ();
-		annualInterestRate = rate;
+		if (rate > 0) {
+			super ();
+			annualInterestRate = rate;
+		else
+			System.println("Error")
+		}
 	}
 
+/**
+Constructor using only the base class with a given rate and balance
+*/
 	SavingsAccount(double balance, double rate) {
 		super (balance);
 		annualInterestRate = rate;
 	}
 
+/**
+Calculates the interest and deposits that amount to the current balance
+*/
 	public void depositMonthlyInterest() {
 		double interest = (this.getBalance() * (annualInterestRate / 100) );
 		this.deposit(interest);
 	}
-	
+
+/**
+Sets an annual interest rate
+*/
 	public void setAnnualInterestRate (double newRate) {
 		annualInterestRate = newRate;
 	}
-	
+
+/**
+Returns the annual interest rate
+*/
 	public double getAnnualInterestRate () {
 		return annualInterestRate;
 	}
