1a36c4a4d67210eb90eae416f278384d06aac93c
wildg <35607715+wildg@users.noreply.github.com>
2018-02-14 15:43:49 -0700

    Update Collectible.java

--- Main Project/AnimationApplicationClass/Collectible.java
+++ Main Project/AnimationApplicationClass/Collectible.java
@@ -1,74 +1,23 @@
-/**
-Creates an object with its length and height an x and y position aIt moves to
-the left of the screen. It checks if it overlaps with the player in the game or
-if the object if off-screen.
-*/
-public class Collectible {
+package game;
 
-	private boolean isAlive;
-	private int xValue;
-	private int yValue;
-	private int length;
-	private int height;
-
-/**
-Constuctor requiring a y value to spawn.
-*/
-	public Collectible(int newYValue) {
-		yValue = newYValue;
-		xValue = 30;
-		height = 1;
-		length = 1;
+public class Collectible extends Items
+{
+	private static int generalHeight = 1;
+	private static int generalLength = 1;
+	
+	public Collectible( int YValue, int height, int length )
+	{
+		super( YValue, height, length );
 	}
-
-/**
-Returns the y position.
-*/
-	public int getYPosition() {
-		return yValue;
+	
+	public Collectible( int YValue )
+	{
+		super( YValue, generalHeight, generalLength );
 	}
-
-/**
-Returns the y position.
-*/
-	public int getXPosition() {
-		return xValue;
-	}
-
-/**
-Returns a boolean of the isAlive.
-*/
-	public boolean getIsAlive() {
-		return isAlive;
-	}
-
-/**
-Returns true if the objects overlaps the given x and y positions and false if
-not.
-*/
-	public boolean overlapsWith(int yPos, int xPos) {
-		if (xPos == this.xValue && yPos == this.yValue) {
-			isAlive = false;
-			return true;
-		}
-		else
-			return false;
-	}
-
-/**
-Returns true if the collectible goes off-screen based on the x position.
-*/
-	public boolean collectibleGone() {
-		if (xValue < 0 )
-			return true;
-		else
-			return false;
-	}
-
-/**
-Moves the collectible towards the left.
-*/
-	public void moveCollectible(){
-		xValue --;
+	
+	public void collision( item[] itemArray, Player player )
+	{
+		if ( super.checkHit(itemArray, player) )
+			player.Heal();
 	}
 }
