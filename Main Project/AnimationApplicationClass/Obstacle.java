671b0b1eff3e7a95c384d3f2dfa0be2c76cb6360
Mark <35700344+markjt17@users.noreply.github.com>
2018-02-14 17:19:03 -0700

    fixed Items array name

--- Main Project/AnimationApplicationClass/Obstacle.java
+++ Main Project/AnimationApplicationClass/Obstacle.java
@@ -2,18 +2,18 @@ public class Obstacle extends Items
 {
 	private static int generalHeight = 1;
 	private static int generalLength = 1;
-	
+
 	public Obstacle( int YValue, int height, int length )
 	{
 		super( YValue, height, length );
 	}
-	
+
 	public Obstacle( int YValue )
 	{
 		super( YValue, generalHeight, generalLength );
 	}
-	
-	public void collision( item[] itemArray, Player player )
+
+	public void collision( Items[] itemArray, Player player )
 	{
 		if ( super.checkHit(itemArray, player) )
 			player.TakeDamage();
