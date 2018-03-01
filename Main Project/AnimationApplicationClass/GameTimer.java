235778ccef1eb0e7b47c41164417ed3080a5c176
markjt17 <mark.tremblay2@ucalgary.ca>
2018-03-01 14:49:11 -0700

    GameTimer V3 Game now runs at 100fps. Added addLimitedEvent method, and other changes.

--- Main Project/AnimationApplicationClass/GameTimer.java
+++ Main Project/AnimationApplicationClass/GameTimer.java
@@ -1,36 +1,38 @@
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
-import javafx.animation.ParallelTransition;
 
+import javafx.animation.ParallelTransition;
+import javafx.animation.Animation;
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 
 import javafx.util.Duration;
 
-// Version 2
-// 1. Added constants to control fps and single frame duration in milliseconds.
-// 2. Added a frameCounter to count frames and getCurrentFrame method.
-// 3. Added static methods to return constants for frame duration and fps, and
-//    static methods to convert second and minutes values to frames based on
-//    framerate of timer, and return integer of that value.
-// 4. Updated javadoc.
+// Version 3
+// 1. Made framerate 100fps to deal with decimal inaccuracy so we deal with only
+//    whole numbers.
+// 2. Made currentFrame instance variable long so it can hold huge integers.
+// 3. added addLimitedEvent method to add an event that fires consistently until
+//    a certain period in seconds elapses.
+// 4. Added game timer pause and stop methods.
 
 /**
  * An of instance of this class creates a group of timers that persistently fire
  * EventHandler action events at specified frame intervals--based on  a game running 
- * at 60fps. All timers run parallel, and only a masterTimer, in which all
+ * at 100fps. All timers run parallel, and only a masterTimer, in which all
  * the timers are stored, has to be started to begin firing events.
  */
 public class GameTimer{
 
-    private static final int framesPerSecond = 60; //
-
-    //Duration is in milliseconds. 1000 milliseconds is 1 second.
-    private static final double singleFrameDurationMilli = 1000/(double)framesPerSecond;;
+    // Set it to 100 so 1 frame is 10ms; therefore we deal with only whole numbers.
+    private static final int framesPerSecond = 100; 
+                                              
+    //Duration is in milliseconds. 1000 milliseconds is 1 second, so 1 frame is 10ms.
+    private static final double singleFrameDurationMilli = 1000/framesPerSecond;
 
     // Frames elapsed after mainTimer starts. Stores int value of current frame.
     // Starts at frame one.
-    private int currentFrame;
+    private long currentFrame;
 
     // The following timer will start counting frames once masterTimer starts,
     // updating the current frame count in currentFrame.
@@ -86,6 +88,7 @@ public class GameTimer{
     /**
      * Static method that converts a number of seconds to frames at specified fps.
      *
+     *
      *@param integer representing a number of seconds.
      *@return integer representing a number of frames input seconds translates to.
      */
@@ -95,7 +98,7 @@ public class GameTimer{
 	    frames = numberOfSec*framesPerSecond;
 	return frames;
     }
-
+    
     /**
      * Static method that converts a number of minutes to frames at specified fps.
      *
@@ -113,7 +116,7 @@ public class GameTimer{
      * Returns an integer representing the current frame of the
      * mainTimer.
      */
-    public int getCurrentFrame(){
+    public long getCurrentFrame(){
 	return currentFrame;
     }
     
@@ -140,16 +143,61 @@ public class GameTimer{
 	Timeline newEventTimer = new Timeline();
       	newEventTimer.setCycleCount(Timeline.INDEFINITE);
 
-	// Choose length of timeline KeyFrame we repeat. One frame at 60fps is 
-	// approx 0.016ms so we multiply fireEventEveryFrames by 0.016ms to get a
-	// duration equivalent to the number of frames we've specified.
-	double keyFrameDuration = (singleFrameDurationMilli)*(double)fireEventEveryFrames;
-	
-	//Add KeyFrame that fires event, event fires at end of KeyFrame duration.
-	newEventTimer.getKeyFrames().add(new KeyFrame(Duration.millis(keyFrameDuration),event));
+	//Keyframe that sets length of cycle.
+	newEventTimer.getKeyFrames().add(new KeyFrame(Duration.millis(singleFrameDurationMilli*fireEventEveryFrames),event));
 
 	//Place timeline in masterTimeline.
 	masterTimer.getChildren().add(newEventTimer);
+    }
+
+    /**
+      * Adds a newEventTimer to the masterTimer that will fire a selected event at 
+      * every specified frame interval, but only for a specified period of time in
+      * seconds.
+      *
+      *@param fireEventEveryFrames
+      *                Integer representing the frame interval before the event is fired.
+      *@param secondsSeriesLasts
+      *                Integer representing how long event will consitently fire for.
+      *@param event
+      *         An event that takes place when the frame interval has ended.
+      */
+
+    public void addLimitedEvent(int fireEventEveryFrames,int secondsSeriesLasts, EventHandler<ActionEvent> event){
+	// Test to make sure number of seconds is >= 0
+	if (secondsSeriesLasts <= 0){
+	    System.out.println("ERROR: Parameter that takes Number of seconds before ending an event series"+
+			       "when using GameLoopTimer.addLimitedEvent, requires an argument" +
+			       "that's a positive integer greater than 0. " + secondsSeriesLasts +
+			       " is not a valid argument.");
+	    System.exit(0);
+	}
+	
+	// Create event as persistent event first.
+	addPersistentEvent(fireEventEveryFrames,event);
+	
+	// Get event that was just added to masterTimer.
+	int eventIndex = masterTimer.getChildren().size()- 1;
+	Animation newEventTimer = masterTimer.getChildren().get(eventIndex);
+
+	int timerCycleDuration = (int)singleFrameDurationMilli*fireEventEveryFrames;
+	int numberOfCycles = (secondsSeriesLasts*framesPerSecond)/(timerCycleDuration/10);
+
+	newEventTimer.setCycleCount(numberOfCycles);
+
+	//FOR DEBUGGING
+	newEventTimer.setOnFinished(new EventHandler<ActionEvent>(){
+		public void handle(ActionEvent event){
+		    //It prints the frame after the animation
+		    //ends, so we subtract 1 to get frame when
+		    //it ended.
+		    //System.out.println(getCurrentFrame()- 1);   
+		}
+	//FOR DEBUGGING
+	//System.out.println(timerCycleDuration);
+	//System.out.println(numberOfCycles);
+
+	    });	
     }
      
     /**
@@ -159,9 +207,14 @@ public class GameTimer{
 	masterTimer.play();
     }
 
-    public static void main(String[]args){
+    public void stopGameTimer(){
+	masterTimer.stop();
+    }
 
-	GameTimer g1 = new GameTimer();
-	System.out.println(GameTimer.getFrameDuration());
+    public void pauseGameTimer(){
+	masterTimer.pause();
+    }
+
+    public static void main(String[]args){
     }
 }
