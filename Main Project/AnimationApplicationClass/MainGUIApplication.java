6358f1eaf422016d11e09a7aff84477e30da00cc
markjt17 <mark.tremblay2@ucalgary.ca>
2018-03-06 16:18:25 -0700

    MAINGUIApplicationUpdateMARCH6
    Made GameTimer and JumpEvent instance variables of
    MainGUIApplication to better fit UML. Fixed indents in
    MainGUIApplication.

--- Main Project/AnimationApplicationClass/MainGUIApplication.java
+++ Main Project/AnimationApplicationClass/MainGUIApplication.java
@@ -12,76 +12,85 @@ import javafx.stage.Stage;
 
 public class MainGUIApplication extends Application 
 {
-	Player player1 = new Player(10, 290, 150, 269, 5);
+    Player player1 = new Player(10, 290, 150, 269, 5);
 	
-	static int width = 864;
-	static int height = 575;
+    static int width = 864;
+    static int height = 575;
 	
-	private double xObstacle = 900;
-	private double yObstacle = 450;
+    private double xObstacle = 900;
+    private double yObstacle = 450;
 
     // How much to adjust position of obstacle
     // each frame.
     private double positionAdjustmentFactor = 30;
 	
-	final double lengthObstacle = 100;
-	final double heightObstacle = 125;
+    final double lengthObstacle = 100;
+    final double heightObstacle = 125;
 	
     static Image chair;
     static Image background;
     static Image obstacle;
     static Image life;
-    
+
+    // Will act as the main game clock. It is used to fire events at specific intervals, and
+    // start events at certain times in the game by referencing timestamps corresponding to the
+    // game clock. Starting the game clock begins the game. The game clock runs at 30fps, and
+    // the format for a timestamp is as follows: 00:00:00;minutes:seconds:frames.
+    private GameTimer mainTimer = new GameTimer();
+
+    // A KeyEvent that makes the Player object given as an argument jump, when the event is fired.
+    private JumpEvent playerJump = new JumpEvent(player1);
+        
     public Boolean hasHit()
     {
-    		double obstacleLeftEdge = xObstacle;
-    		double obstacleRightEdge = (xObstacle + lengthObstacle);
-    		double obstacleUpperEdge = yObstacle;
-    		double obstacleLowerEdge = (yObstacle + heightObstacle);
+	double obstacleLeftEdge = xObstacle;
+	double obstacleRightEdge = (xObstacle + lengthObstacle);
+	double obstacleUpperEdge = yObstacle;
+	double obstacleLowerEdge = (yObstacle + heightObstacle);
     		
-    		double playerLeftEdge = player1.getXPosition();
-    		double playerRightEdge = (player1.getXPosition() + player1.getPlayerLength());
-    		double playerLowerEdge = (player1.getYPosition() + player1.getPlayerHeight());
+	double playerLeftEdge = player1.getXPosition();
+	double playerRightEdge = (player1.getXPosition() + player1.getPlayerLength());
+	double playerLowerEdge = (player1.getYPosition() + player1.getPlayerHeight());
 
-    		if ((playerLeftEdge <= obstacleRightEdge) && (playerLeftEdge >= obstacleLeftEdge))
-    		{
-    			if ((playerLowerEdge <= obstacleLowerEdge) && (playerLowerEdge >= obstacleUpperEdge))
-    			{
-    				return true;
-    			}
+	if ((playerLeftEdge <= obstacleRightEdge) && (playerLeftEdge >= obstacleLeftEdge))
+	    {
+		if ((playerLowerEdge <= obstacleLowerEdge) && (playerLowerEdge >= obstacleUpperEdge))
+		    {
+			return true;
+		    }
     			
-    			else
-    			{
-    				return false;
-    			}
-    		}
+		else
+		    {
+			return false;
+		    }
+	    }
     		
-    		else if ((playerRightEdge <= obstacleRightEdge ) && (playerRightEdge >= obstacleLeftEdge))
-    		{	
-    			if ((playerLowerEdge <= obstacleLowerEdge) && (playerLowerEdge >= obstacleUpperEdge))
-    			{
-    				return true;
-    			}
+	else if ((playerRightEdge <= obstacleRightEdge ) && (playerRightEdge >= obstacleLeftEdge))
+	    {	
+		if ((playerLowerEdge <= obstacleLowerEdge) && (playerLowerEdge >= obstacleUpperEdge))
+		    {
+			return true;
+		    }
     			
-    			else
-    			{
-    				return false;
-    			}
-    		}
+		else
+		    {
+			return false;
+		    }
+	    }
     		
-    		else
-    		{
-    			return false;
-    		}
+	else
+	    {
+		return false;
+	    }
     }
-	public static void main(String[] args) {
-		launch(args);
-	}
+    public static void main(String[] args) {
+	launch(args);
+    }
 	
-	/* 
-	 * The following two methods help pull the images needed to display. These methods were taken from:
-	 * https://gamedevelopment.tutsplus.com/tutorials/introduction-to-javafx-for-game-development--cms-23835
-	 */
+    /* 
+     * The following two methods help pull the images needed to display. These methods were taken from:
+     * https://gamedevelopment.tutsplus.com/tutorials/introduction-to-javafx-for-game-development--cms-23835
+     */
 	
     private static void loadGraphics()
     {
@@ -96,21 +105,17 @@ public class MainGUIApplication extends Application
         return MainGUIApplication.class.getResource(filename).toString();
     }
     
-	public void start(Stage primaryStage) throws Exception
-	{
-	    loadGraphics();
+    public void start(Stage primaryStage) throws Exception
+    {
+	loadGraphics();
 	    
-		Group root = new Group();
-		Scene scene = new Scene(root);
-		Canvas canvas = new Canvas(width, height);
-		GraphicsContext gc = canvas.getGraphicsContext2D();
+	Group root = new Group();
+	Scene scene = new Scene(root);
+	Canvas canvas = new Canvas(width, height);
+	GraphicsContext gc = canvas.getGraphicsContext2D();
 			        				
-		scene.setOnKeyPressed(new JumpEvent(player1));
-        	
-	// Initialize timer to generate events at
-	// certain in game frames. Runs at 60fps.
-	GameTimer mainTimer = new GameTimer();
-	
+	scene.setOnKeyPressed(playerJump);
+               	
 	// Update screen and check for collisions. Set start to 0 seconds of gameClock,
 	// and set interval to 1 frame. Therefore event will fire indefinitely, once
 	// every frame, starting at time 0.
@@ -146,9 +151,9 @@ public class MainGUIApplication extends Application
 
 	mainTimer.startGameTimer();
 
-		root.getChildren().add(canvas);
-		primaryStage.setTitle("HIS RIGHTFUL PLACE");
-		primaryStage.setScene(scene);
-		primaryStage.show();
-	}
+	root.getChildren().add(canvas);
+	primaryStage.setTitle("HIS RIGHTFUL PLACE");
+	primaryStage.setScene(scene);
+	primaryStage.show();
+    }
 }
